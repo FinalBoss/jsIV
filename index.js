1ac2abcffd163714f003880fc4d91fbc8ea39ab7
FinalBoss <finalb@projectfinalboss.com>
2019-12-19 17:23:50 -0500

    Finish 1-3(edit), work on 4

--- index.js
+++ index.js
@@ -168,7 +168,28 @@ speak() {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
+class Instructor extends Lambdasian {
+
+  constructor (attribute) {
+    super(attribute);
+
+this.specialty = attribute.specialty;
+this.favLanguage = attribute.favLanguage;
+this.catchPhrase = attribute.catchPhrase;
+
+  }
+demo(subject) {
+
+  return `Today we are learning about ${attribute.subject}`
+
+
+}
+
+grade(student, subject) {
+
+  return `${student.name} receives a perfect score on ${attribute.subject}`
+
+}
 
 }
 
